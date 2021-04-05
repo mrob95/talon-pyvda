69905efc3231ede393ffe7577e168f94796ca7fc
Mike Roberts <42875462+mrob95@users.noreply.github.com>
2021-04-05 12:07:38 +0100

    Updates for 0.1.0

--- workspace.py
+++ workspace.py
@@ -4,21 +4,10 @@ import logging
 
 # https://github.com/mrob95/py-VirtualDesktopAccessor
 # talon/bin/pip.bat install pyvda
-import pyvda
-
-ASFW_ANY = -1
+from pyvda import AppView, get_apps_by_z_order, VirtualDesktop
 
 # Disable log spam
 logging.getLogger("comtypes").setLevel(logging.INFO)
-
-def go_to_n(n):
-    windll.user32.AllowSetForegroundWindow(ASFW_ANY)
-    pyvda.GoToDesktopNumber(n)
-
-def get_windows_by_z():
-    windows = {w.id: w for w in ui.windows()}
-    windows_by_z = [windows[z] for z in pyvda.ViewGetByZOrder()]
-    return windows_by_z
 
 mod = Module()
 
@@ -26,52 +15,50 @@ mod = Module()
 class Actions:
     def workspace_send(n: int):
         """Send the current window to a given workspace"""
-        wndh = ui.active_window().id
-        pyvda.MoveWindowToDesktopNumber(wndh, n)
+        AppView.current().move(VirtualDesktop(n))
 
     def workspace_move(n: int):
         """Send the current window to a given workspace, and follow it"""
-        wndh = ui.active_window().id
-        pyvda.MoveWindowToDesktopNumber(wndh, n)
-        go_to_n(n)
+        target = VirtualDesktop(n)
+        AppView.current().move(target)
+        target.go()
 
     def workspace_go(n: int):
         """Go to a given workspace"""
-        go_to_n(n)
+        VirtualDesktop(n).go()
 
     def workspace_next(n: int):
         """Go to the next workspace (without animation)"""
-        current = pyvda.GetCurrentDesktopNumber()
-        go_to_n(current+n)
+        current = VirtualDesktop.current()
+        VirtualDesktop(current.number + 1).go()
 
     def workspace_previous(n: int):
         """Go to the previous workspace (without animation)"""
-        current = pyvda.GetCurrentDesktopNumber()
-        go_to_n(current-n)
+        current = VirtualDesktop.current()
+        VirtualDesktop(current.number - 1).go()
 
     def window_pin():
         """Pin the current window, making it active in all workspaces"""
-        wndh = ui.active_window().id
-        pyvda.PinWindow(wndh)
+        AppView.current().pin()
 
     def window_unpin():
         """Unpin the current window"""
-        wndh = ui.active_window().id
-        pyvda.UnPinWindow(wndh)
+        AppView.current().unpin()
 
     def window_next(n: int):
         """Switch to the window that is `n` windows beneath the active window"""
-        windows_by_z = get_windows_by_z()
+        windows = {w.id: w for w in ui.windows()}
+        windows_by_z = get_apps_by_z_order()
         target = windows_by_z[n % len(windows_by_z)]
-        target.app.focus()
-        pyvda.ViewSwitchTo(target.id)
+        windows[target.hwnd].focus()
+        target.switch_to()
 
     def window_focus_pos(x: int, y: int):
         """Switch to the highest window that contains the point (x, y)"""
         windows = {w.id: w for w in ui.windows()}
-        windows_by_z = [windows[hwnd] for hwnd in pyvda.ViewGetByZOrder()]
-        for w in windows_by_z:
-            if w.rect.contains(x, y):
-                w.focus()
-                pyvda.ViewSwitchTo(w.id)
+        for a in get_apps_by_z_order():
+            if windows[a.hwnd].rect.contains(x, y):
+                windows[a.hwnd].focus()
+                # This seems more robust
+                a.switch_to()
                 return
